15aa0116243fd612cbd38cf09ae391cd8db1587e
mszost <mszo@tuta.io>
2023-10-12 16:06:57 -0500

    fix indentation

--- init.py
+++ init.py
@@ -3,9 +3,9 @@ import os
 from flask import Flask
 
 def create_app(test_config=None):
-  # create and configure the app
-  app = Flask(__name__, instance_relative_config=True)
-  app.config.from_mapping(
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
     SECRET_KEY='dev',
     DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
@@ -24,8 +24,7 @@ def create_app(test_config=None):
         pass
 
     # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
-
+@app.route('/hello')
+def hello():
+    return 'Hello, World!'
     return app
