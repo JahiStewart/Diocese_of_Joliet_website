8c53944915bd333ba682fa042411171d7be28dae
Chris Martinez <chmartinez2014@gmail.com>
2023-12-01 01:51:08 -0600

    Fixed Reservation form

--- flaskr/forms.py
+++ flaskr/forms.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, redirect, url_for, render_template, request, session
 from flask_wtf import FlaskForm
-from wtforms import StringField, BooleanField, SelectField, SubmitField, DateField, RadioField
+from wtforms import StringField, BooleanField, SelectField, SubmitField, DateField, RadioField, IntegerField
 from wtforms.validators import InputRequired
 from flaskr.db import get_db
 from uuid import uuid4
@@ -10,14 +10,14 @@ from datetime import datetime
 forms = Blueprint('form', __name__)
 
 class RoomRequestForm(FlaskForm):
-    date = DateField('', validators=[InputRequired()])
-    start_time = SelectField('', choices=['06:00 AM', '06:30 AM', '07:00 AM', '07:30 AM', '08:00 AM', '08:30 AM', '09:00 AM', '09:30 AM', '10:00 AM', '10:30 AM', '11:00 AM', '11:30 AM', '12:00 PM', '12:30 PM', '01:00 PM', '01:30 PM', '02:00 PM', '02:30 PM', '03:00 PM', '03:30 PM', '04:00 PM', '04:30 PM', '05:00 PM', '05:30 PM', '06:00 PM', '06:30 PM', '07:00 PM', '07:30 PM', '08:00 PM'])
-    end_time = SelectField('', choices=['06:00 AM', '06:30 AM', '07:00 AM', '07:30 AM', '08:00 AM', '08:30 AM', '09:00 AM', '09:30 AM', '10:00 AM', '10:30 AM', '11:00 AM', '11:30 AM', '12:00 PM', '12:30 PM', '01:00 PM', '01:30 PM', '02:00 PM', '02:30 PM', '03:00 PM', '03:30 PM', '04:00 PM', '04:30 PM', '05:00 PM', '05:30 PM', '06:00 PM', '06:30 PM', '07:00 PM', '07:30 PM', '08:00 PM'])
+    date = DateField('Date', validators=[InputRequired()])
+    start_time = SelectField('Start', choices=['06:00 AM', '06:30 AM', '07:00 AM', '07:30 AM', '08:00 AM', '08:30 AM', '09:00 AM', '09:30 AM', '10:00 AM', '10:30 AM', '11:00 AM', '11:30 AM', '12:00 PM', '12:30 PM', '01:00 PM', '01:30 PM', '02:00 PM', '02:30 PM', '03:00 PM', '03:30 PM', '04:00 PM', '04:30 PM', '05:00 PM', '05:30 PM', '06:00 PM', '06:30 PM', '07:00 PM', '07:30 PM', '08:00 PM'])
+    end_time = SelectField('End', choices=['06:00 AM', '06:30 AM', '07:00 AM', '07:30 AM', '08:00 AM', '08:30 AM', '09:00 AM', '09:30 AM', '10:00 AM', '10:30 AM', '11:00 AM', '11:30 AM', '12:00 PM', '12:30 PM', '01:00 PM', '01:30 PM', '02:00 PM', '02:30 PM', '03:00 PM', '03:30 PM', '04:00 PM', '04:30 PM', '05:00 PM', '05:30 PM', '06:00 PM', '06:30 PM', '07:00 PM', '07:30 PM', '08:00 PM'])
 
     arrangement = RadioField('Room Arrangement:', choices=["", "", ""])
 
     purpose = StringField('Purpose:', validators=[InputRequired()])
-    attendees = SelectField('Number of Attendees', choices=["",1,2,3,4,5,6,7,8,9,10], validators=[InputRequired()]) # make a function to pull the values in the array from the max occupancy of the room 
+    attendees = IntegerField('Number of Attendees', validators=[InputRequired()]) # make a function to pull the values in the array from the max occupancy of the room 
 
     coffee = BooleanField('Coffee')
     soda = BooleanField('Soda')
@@ -46,12 +46,12 @@ class RoomRequestForm(FlaskForm):
 def book(room_id):
     # get date, start time, and end time from session
     date = session.get('date')
-    print(date, type(date))
+    date = datetime.strptime(date[5:16], '%d %b %Y')
     start_time = session.get('start_time')
     end_time = session.get('end_time')
 
     # create form with date, start time, and end time pre-populated
-    form = RoomRequestForm(start_time=start_time, end_time=end_time)
+    form = RoomRequestForm(date=date, start_time=start_time, end_time=end_time)
 
     db = get_db()
 
@@ -71,23 +71,27 @@ def book(room_id):
         if form.cancel.data:  # if cancel button is clicked, the form.cancel.data will be True
             return redirect(url_for('form.index')) # go back to previous page (change to room select once that page exists)
 
-        org_id = request.form['org_id']
-        print(org_id)
-        room_arrangement_id = request.form['arrangement']
+        org_id = request.form.get('org_id')
+        room_arrangement_id = request.form.get('arrangement')
 
 
         # export all of the data from this form into the database
         reservation_id = str(uuid4())
         db.execute(
-            '''INSERT INTO reservation 
-            (Id, User_id, Res_Date, Beg_Time, End_Time, Purpose, Organization_Id, Attendee_Count,Coffee_YN, Soda_YN, Water_YN, 
-            Catering_YN, Lapel_Mic, Podium_Mic, Handheld_Mic, Conf_Phone, Whiteboard_YN, Easel_YN, Special_Notes, Lobby_YN, Room_Id, RoomArrangement_Id) 
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''', 
-            (reservation_id, session.get('user_id'), form.date.data, form.start_time.data, form.end_time.data, form.purpose.data, org_id, form.attendees.data, 
-             form.coffee.data, form.soda.data, form.water.data, form.catering.data, form.lapel_mic.data, form.podium_mic.data, 
-            form.handheld_mic.data, form.conference_phone.data, form.whiteboard.data, form.easel.data, form.notes.data, form.no_lobby.data, room_id, room_arrangement_id),)
+            '''INSERT INTO Reservation 
+            (Id, User_id, Res_Date, Beg_Time, End_Time, Purpose, Organization_Id, Attendee_Count, Coffee_YN, Soda_YN, 
+            Water_YN, Catering_YN, Lapel_Mic, Podium_Mic, Handheld_Mic, Conf_Phone, Whiteboard_YN, Easel_YN, Special_Notes, Lobby_YN,
+            Room_Id, RoomArrangement_Id) 
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''', 
+            (reservation_id, session.get('user_id'), form.date.data, form.start_time.data, form.end_time.data, form.purpose.data, org_id, form.attendees.data, form.coffee.data, form.soda.data, 
+            form.water.data, form.catering.data, form.lapel_mic.data, form.podium_mic.data, form.handheld_mic.data, form.conference_phone.data, form.whiteboard.data, form.easel.data, form.notes.data, form.no_lobby.data, 
+            room_id, room_arrangement_id),)
+        db.commit()
+        
+        return redirect(url_for('form.index')) # go back to previous page (change to confirmation page or myaccount page)
 
-    return render_template('form.html', form=form, room=room, organizations=organizations, room_arrangements=room_arrangements)
+
+    return render_template('book.html', form=form, room=room, organizations=organizations, room_arrangements=room_arrangements)
 
 
 @forms.route('/', methods=['GET', 'POST'])
@@ -96,12 +100,13 @@ def index():
 
     if request.method =='POST':
         # get results from database
-        results = get_results(form.date.data, form.start_time.data, form.end_time.data)
+        results = get_results(form.attendees.data, form.start_time.data, form.end_time.data)
 
         # store date, start time, and end time in session
         session['date'] = form.date.data
         session['start_time'] = form.start_time.data
         session['end_time'] = form.end_time.data
+        session['attendees'] = form.attendees.data
 
         return render_template('results.html', results=results)
 
@@ -109,10 +114,12 @@ def index():
 
 
 
-def get_results(date, start_time, end_time):
+def get_results(attendees, start_time, end_time):
     db = get_db()
 
     # get all rooms that are not reserved during the specified time
-    available_rooms = db.execute('''SELECT * FROM room WHERE Id NOT IN 
-                                (SELECT Room_Id FROM reservation WHERE Res_Date = ? AND Beg_Time <= ? AND End_Time >= ?)''', (date, end_time, start_time)).fetchall()
+    available_rooms = db.execute('''SELECT * from room WHERE capacity >= ? AND Id NOT IN 
+                                (SELECT Room_Id FROM reservation WHERE Beg_Time >= ? AND Beg_Time <= ?
+                                 OR End_Time >= ? AND End_Time <= ?
+                                 OR Beg_Time <= ? AND End_Time >= ?)''', (attendees, start_time, end_time, start_time, end_time, start_time, end_time)).fetchall()
     return available_rooms
